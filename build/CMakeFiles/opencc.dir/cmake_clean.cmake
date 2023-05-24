9194129f6599ccafd5b7d1805740655baea8e865
Gürsel Türkeri <gurselt33@gmail.com>
2023-05-24 20:35:57 +0300

    Add files via upload

---
+++ build/CMakeFiles/opencc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencc.dir/opencc.cpp.o"
+  "CMakeFiles/opencc.dir/opencc.cpp.o.d"
+  "opencc"
+  "opencc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
